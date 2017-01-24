f8fb7dad61430be1874ec57861ca810a7d529fa8
Tarcisio Costa Corte de Paula <tarcisiocorte@gmail.com>
2017-01-24 17:02:20 +0000

    Arrays - DS

---
+++ datastructure/src/hackerrank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace hackerran
+{
+    class Solution
+    {
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+            for (int a = n - 1; a >= 0; --a)
+            {
+                Console.Write(arr[a] + " ");
+            }
+        }
+    }
+}
---
+++ datastructure/src/hackerrank/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("hackerrank")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2fe9e5ea-5b06-4ead-aba0-10bc0f5022e5")]
